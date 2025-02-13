4e9d2e511880659172e6f852475caf9b0a45bc85
Aziz <azizbek.k@wiztechgroup.com>
2025-02-13 22:40:20 +0500

    make navbar fixed

--- public/js/app.js
+++ public/js/app.js
@@ -49,5 +49,17 @@ function playVideo() {
   });
 }
 
+function setNavbarBG() {
+  const header = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    header.classList.add("bg-black/90", "shadow-lg", "backdrop-blur");
+  } else {
+    header.classList.remove("bg-black/90", "shadow-lg", "backdrop-blur");
+  }
+}
+
+window.addEventListener("scroll", setNavbarBG);
+window.addEventListener("load", setNavbarBG);
+
 document.querySelector('#headerHamburgerBtm') && headerMenu();
 document.querySelector('#playButton') && playVideo();
