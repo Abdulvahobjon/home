b868d23b761f31c149dac17274633335dc74f82b
bexruz-cyber <mayoq2024@gmail.com>
2025-02-15 00:48:32 +0500

    add projects.html

---
+++ public/js/Project.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+let swiper_pagination_bullet = document.querySelectorAll(
+  ".swiper-pagination-bullet"
+);
+
+const Swiper_pagination_bulletFun = () => {
+  swiper_pagination_bullet[0].textContent = "Images";
+  swiper_pagination_bullet[1].textContent = "News";
+  swiper_pagination_bullet[2].textContent = "Gallery";
+  swiper_pagination_bullet[3].textContent = "Videos";
+};
+Swiper_pagination_bulletFun();
